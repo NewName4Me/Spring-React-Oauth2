81314b0414d81355347669b0f96b968ac25b6c3b
NewName4Me <tortarod@gmail.com>
2024-09-16 20:29:33 +0200

    añadir cross origin adecuado para mostrar la informacion del usuario2

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -9,6 +9,7 @@ function Dashboard() {
         axios.get("http://localhost:8080/user-info", { withCredentials: true })
             .then(response => {
                 setUser(response.data);
+                console.log(response.data);
             })
             .catch(error => {
                 console.log("Ocurrio un error:", error);
@@ -22,6 +23,7 @@ function Dashboard() {
                 <div>
                     <p>Nombre: {user.name}</p>
                     <p>Email: {user.email}</p>
+                    {user.picture && <img src={user.picture} alt="Avatar" referrerPolicy='no-referrer'/>}
                 </div>
             ) : (
                 <p>Cargando información del usuario</p>
