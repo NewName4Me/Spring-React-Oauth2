0b2304df7e4f50eed20f497e27f7f408d9cc3bbe
NewName4Me <tortarod@gmail.com>
2024-09-16 18:44:46 +0200

    crear formulario basico spring

---
+++ oauth/oauth/src/main/java/com/tortarod/oauth/GreetinsController.java
@@ -0,0 +1,14 @@
+package com.tortarod.oauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetinsController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
+}
